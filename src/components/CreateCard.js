831b9cd8fbf3b0c42519f49046ca95ed6842303e
CloudigAround <sherellie1@hotmail.com>
2023-02-12 14:55:42 +0000

    space added to ingredients

--- src/components/CreateCard.js
+++ src/components/CreateCard.js
@@ -8,35 +8,35 @@ export default function CreateCard(props) {
        <ul className="card-list">
          {props && (
            <li>
-             {props.strMeasure1} {props.strIngredient1}
+             {props.strMeasure1}      {props.strIngredient1}
  
            </li>
          )}
          {props && (
            <li>
-             {props.strMeasure2} {props.strIngredient2}
+             {props.strMeasure2}       {props.strIngredient2}
            </li>
           )}
           {props && (
            <li>
  
-            {props.strMeasure3} {props.strIngredient3}
+            {props.strMeasure3}      {props.strIngredient3}
            </li>
          )}
          {props && (
  
            <li>
-             {props.strMeasure4} {props.strIngredient4}
+             {props.strMeasure4}      {props.strIngredient4}
            </li>
          )}
          {props && (
            <li>
-             {props.strMeasure5} {props.strIngredient5}
+             {props.strMeasure5}      {props.strIngredient5}
            </li>
          )}
          {props && (
            <li>
-             {props.strMeasure6} {props.strIngredient6}
+             {props.strMeasure6}      {props.strIngredient6}
            </li>
          )}
          <h3 className="subTitle">Shaken it or Stir it?</h3>
